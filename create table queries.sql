276cc81d2434ff198ea401fa9eb7089e3c2fdfb5
scaraclette <gustiscarlett@gmail.com>
2021-05-30 16:33:12 -0700

    current creation

--- create table queries.sql
+++ create table queries.sql
@@ -38,9 +38,14 @@ CREATE TABLE Venue (
 
 */
 
--- TABLENAME Table
+-- TABLENAME Table: Scarlett
 CREATE TABLE Exhibit (
-
+  Name varchar(150) NOT NULL,
+  Description varchar(MAX),
+  StartDate date,
+  EndDate date,
+  VenueID int FOREIGN KEY REFERENCES Venue(VenueID) NOT NULL
+  CONSTRAINT PK_Exhibit PRIMARY KEY (VenueID, Name, StartDate)
 )
 
 /*
@@ -52,7 +57,10 @@ CREATE TABLE Exhibit (
 
 -- TABLENAME Table
 CREATE TABLE Art_Style (
-
+  Name varchar(150) NOT NULL,
+  Period varchar(MAX),
+  Addl_Info varchar(MAX)
+  CONSTRAINT PK_Art_Style PRIMARY KEY (Name, Period)
 )
 
 /*
@@ -64,7 +72,16 @@ CREATE TABLE Art_Style (
 
 -- TABLENAME Table
 CREATE TABLE Artwork (
-
+  Title varchar(150) NOT NULL,
+  CreationDate Date NOT NULL,
+  ArtworkValue int NOT NULL,
+  CONSTRAINT ARTWORK_VALUE_CHK_ID CHECK (ArtworkValue>=0),
+  ArtistID int FOREIGN KEY REFERENCES Artist(ArtistID) NOT NULL,
+  Addl_Info varchar(MAX),
+  VenueID int FOREIGN KEY REFERENCES Venue(VenueID) NOT NULL,
+  StyleName varchar(150) FOREIGN KEY REFERENCES Art_Style(Name),
+  Period varchar(MAX) FOREIGN KEY REFERENCES Art_Style(Table)
+  CONSTRAINT PK_Artwork PRIMARY KEY (Title, CreationDate, ArtistID)
 )
 
 /*
@@ -76,7 +93,10 @@ CREATE TABLE Artwork (
 
 -- TABLENAME Table
 CREATE TABLE Artwork_Creation (
-
+  ArtistID int FOREIGN KEY REFERENCES Artist(ArtistID) NOT NULL,
+  Title varchar(150) FOREIGN KEY REFERENCES Artwork(Title) NOT NULL,
+  CreationDate Date FOREIGN KEY REFERENCES Artwork(CreationDate) NOT NULL
+  CONSTRAINT PK_Artwork_Creation PRIMARY KEY (ArtistID, Title, CreationDate)
 )
 
 /*
@@ -100,7 +120,13 @@ CREATE TABLE Art_Ownership (
 
 -- TABLENAME Table
 CREATE TABLE Exhibit_Contents (
-
+  VenueID int FOREIGN KEY REFERENCES Venue(VenueID) NOT NULL,
+  Name varchar(150) FOREIGN KEY REFERENCES Exhibit(Name) NOT NULL,
+  StartDate Date FOREIGN KEY REFERENCES Exhibit(StartDate) NOT NULL,
+  Title varchar(150) FOREIGN KEY REFERENCES Artwork(Title) NOT NULL,
+  CreationDate Date FOREIGN KEY REFERENCES Artwork(CreationDate) NOT NULL,
+  ArtistID int FOREIGN KEY REFERENCES Artist(ArtistID) NOT NULL,
+  CONSTRAINT PK_Exhibit_Contents PRIMARY KEY (VenueID, Name, StartDate, Title, CreationDate, ArtistID)
 )
 
 /*
